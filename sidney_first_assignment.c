e2ce31c0e351991915a7a25171fd58530497919c
SidAndreo122 <sidney122@comcast.net>
2025-08-22 17:10:54 +0000

    a

---
+++ sidney_first_assignment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    char firstLetter = 'A';
+    int age = 21;
+    float height = 6.2;
+    bool isStudent = true;
+
+    printf("My first letter of my last name is: %\n", firstLetter);
+    return 0;
+}
