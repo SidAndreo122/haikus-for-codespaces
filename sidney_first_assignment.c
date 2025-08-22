68eee9d419f4aebd28893efdd325a2c2399f6645
SidAndreo122 <sidney122@comcast.net>
2025-08-22 17:22:54 +0000

    submit assignment for review

--- sidney_first_assignment.c
+++ sidney_first_assignment.c
@@ -7,6 +7,6 @@ int main() {
     float height = 6.2;
     bool isStudent = true;
 
-    printf("My first letter of my last name is: %\n", firstLetter);
+    printf("My first letter of my last name is: %c\n My age is %d\n My height is %.2f\n Finally am I a student? 1(True) or 0(False)? %d\n", firstLetter, age, height, isStudent);
     return 0;
 }
