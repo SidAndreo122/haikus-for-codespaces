3fef5eafed125b3f796e0ffdfb83820e836a162a
SidAndreo122 <sidney122@comcast.net>
2025-08-25 16:44:25 +0000

    submit assignment for review

---
+++ sidney_first_hw.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    char firstLetter;
+    int age;
+    float height;
+    bool isStudent;
+
+    // scanf("format specifier", &variable")
+    printf("Enter your first letter of your last name: ");
+    scanf("%c", &firstLetter);
+    printf("Enter your age:");
+    scanf("%d", &age);  
+    printf("Enter your height (format it as foot.inches please)");
+    scanf("%f", &height);
+    printf("Are you a student? (Enter 1 for True, or enter 0 for false)");
+    scanf("%d", &isStudent);
+    // printf("Message+format specififer", variables);
+    printf("Your first letter of your last name is: %c\n Your age is %d\n Your height is %.2f\n Finally are you are student? 1(True) or 2(False)? %d\n", firstLetter, age, height, isStudent);
+    return 0;
+}
