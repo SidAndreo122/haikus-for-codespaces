77d5a951569a6ca356a9145ce1c3d59763cec6b6
SidAndreo122 <sidney122@comcast.net>
2025-10-06 16:36:31 +0000

    submit for review

--- sidney_eight_assignment.c
+++ sidney_eight_assignment.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+int ties = 0;
+int player_wins = 0; 
+int computer_wins = 0;
+char play_again = 'Y';
 int getComputerChoice() {
 
     int x = (rand()%3)+1;
@@ -16,9 +20,22 @@ char decideWinner(int player, int computer){
     return -1;
 }
 
+void choiceDecrypt(int ch){
+    if(ch == 1){
+        printf(" Rock");
+    }
+    else if(ch == 2){
+        printf(" Paper");
+    }
+    else if(ch == 3){
+        printf(" Scissors");
+    }
+}
+
 void main() {
     int input;
-    do {
+    while (play_again == 'Y'){
+        do {
         printf("Choose a match length: 5 or 10\n");
         scanf(" %d", &input);
         // if (input != 5 || input != 10){
@@ -27,28 +44,55 @@ void main() {
     } while (input != 5 && input != 10);
     for (int i = 1; i <= input; i++){
         int user_input;
-        printf("Round %d", i);
+        printf("Round %d\n", i);
         printf("Enter 1= Rock, Enter 2= Paper, Enter 3 = Scissors\n");
         scanf("%d", &user_input);
-        switch (user_input){
+        int x = getComputerChoice();
+        int result = decideWinner(user_input, x);
+        printf("You chose");
+        choiceDecrypt(user_input);
+        printf(",");
+        printf("Computer chose");
+        choiceDecrypt(x);
+        printf("\n");
+        switch (result){
+            case 0:
+                
+                printf("It was a tie!");
+                ties++;
+                break;
+              
             case 1:
-                int x = getComputerChoice();
-                printf("You chose Rock, Computer chose %d", x);
-                int result = decideWinner(user_input, x);
-            case 2:
-                int x = getComputerChoice();
-                printf("You chose Paper, Computer chose %d", x);
-                int result = decideWinner(user_input, x);
-            case 3:
-                int x = getComputerChoice();
-                printf("You choose Scissors, Computer chose %d", x);
-                int result = decideWinner(user_input, x);
-                switch (result) {
-                    case 0:
-                        printf("It was a tie!");
-                        
-                }
+                
+                printf("You win this round!");
+                player_wins++;
+                break;
+                
+            default:
+                printf("Computer wins!");
+                computer_wins++;
+                break;
+        }
+        
+    }
+    printf("\n=== Final Results ===\n");
+        printf("You: %d Computer: %d Ties %d\n", player_wins, computer_wins, ties);
+        if (player_wins > computer_wins) {
+            printf("You wins the match!\n");
+        }
+        else if (computer_wins > player_wins){
+            printf("Computer wins the match!\n");
+        }
+        else {
+            printf("It's a draw");
+        }
+        
+        printf("Play again? (Y/N)");
+        scanf(" %c", &play_again);
+        if (play_again == 'N') {
+            printf("Thanks for playing!");
+            break;
         }
     }
-
+    
 }
