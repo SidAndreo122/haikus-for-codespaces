7c9b3eb187dd3833ebb4296d44a3403263008e79
SidAndreo122 <sidney122@comcast.net>
2025-10-22 16:46:48 +0000

    add test files

--- pointer_test.c
+++ pointer_test.c
@@ -14,4 +14,20 @@ void main() {
     // PC doesnt know the value of a variable it knows the address of a variable. The compilier translates
     *ptr = 20;
     printf("%d", myAge); // you can change a value of a variable with pointers
+    int val =5;
+    int *val_ptr = &val;
+
+    printf("The address of val = %p \n", val_ptr);
+    printf("The address of val_ptr = %p\n", &val_ptr);
+    printf("The vlaue stored in val_ptr %p\n", val_ptr);
+    printf("The value stored in ptr = %d\n", *val_ptr);
+
+    char vala ='a';
+    char *val_ptra = &vala;
+
+    printf("The address of val = %p\n", val_ptra);
+    printf("The address of val_ptr = %p\n", &val_ptra);
+    printf("The vlaue stored in val_ptr %p\n", val_ptra);
+    printf("The value stored in ptr = %c\n", *val_ptra);
+    printf("%d", sizeof(val_ptra));
 }
