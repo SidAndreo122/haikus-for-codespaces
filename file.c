a6c20625faccea65b4ed1cb32162e04e365a31dd
SidAndreo122 <sidney122@comcast.net>
2025-11-17 17:46:44 +0000

    I/O notes

---
+++ file.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    FILE * my_file;
+    // file is a datatype
+    // my_file = fopen("file name/location", "permissions")
+    // \0 for strings and EOF for files
+    // fgetc reads
+    // fputc writes
+    // fscanf reads
+    // fprintf writes
+    // Sequential files
+    // making structure for files such as csv where commas are needed to separate
+}
